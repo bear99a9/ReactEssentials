63a2f8164a8c19e8d05252449f40bd552114784e
Sean Edwards <sean.edwards@houseful.co.uk>
2025-01-18 11:53:02 +0000

    Rendering content conditionally first example

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import CoreConcept from "./components/CoreConcepts.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState('components');
+  const [ selectedTopic, setSelectedTopic ] = useState();
 
   function handleClick(selectedButton) {
     setSelectedTopic(selectedButton)
@@ -34,13 +34,15 @@ function App() {
               <TabButton onClick={() => handleClick('state')}>State</TabButton>
             </menu>
           </h2>
-          <div id="tab-content">
+          {!selectedTopic ?  <p>Please select a topic.</p> : 
+          (<div id="tab-content">
             <h3>{EXAMPLES[selectedTopic].title}</h3>
             <p>{EXAMPLES[selectedTopic].description}</p>
             <pre>
               <code>{EXAMPLES[selectedTopic].code}</code>
             </pre>
-          </div>
+          </div> 
+          )}
         </section>
       </main>
     </div>
