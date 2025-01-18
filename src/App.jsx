4681fa088854b11ddfc1f4d7de7c5cdaa403dc2c
Sean Edwards <sean.edwards@houseful.co.uk>
2025-01-18 11:54:53 +0000

    Rendering content conditionally second example - && not tierney

--- src/App.jsx
+++ src/App.jsx
@@ -34,15 +34,15 @@ function App() {
               <TabButton onClick={() => handleClick('state')}>State</TabButton>
             </menu>
           </h2>
-          {!selectedTopic ?  <p>Please select a topic.</p> : 
-          (<div id="tab-content">
+          {!selectedTopic && <p>Please select a topic.</p>}
+          {selectedTopic && (<div id="tab-content">
             <h3>{EXAMPLES[selectedTopic].title}</h3>
             <p>{EXAMPLES[selectedTopic].description}</p>
             <pre>
               <code>{EXAMPLES[selectedTopic].code}</code>
             </pre>
           </div> 
-          )}
+          ) }
         </section>
       </main>
     </div>
